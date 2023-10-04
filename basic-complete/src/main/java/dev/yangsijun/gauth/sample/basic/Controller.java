adb3477ac392f16d1b3b032f3f43b1acc2b4b0ba
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-04 11:54:05 +0900

    basic 예시 완성본 구현

---
+++ basic-complete/src/main/java/dev/yangsijun/gauth/sample/basic/Controller.java
@@ -0,0 +1,37 @@
+package dev.yangsijun.gauth.sample.basic;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/role/student")
+    public String student() {
+        return "hi student!";
+    }
+
+    @GetMapping("/role/teacher")
+    public String teacher() {
+        return "hi teacher!";
+    }
+
+    @GetMapping("/auth/info/me")
+    public String info() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return auth.getPrincipal().toString();
+    }
+
+    @GetMapping("/auth/authority/me")
+    public String authority() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return auth.getAuthorities().toString();
+    }
+
+    @GetMapping("/token")
+    public String token() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return auth.toString();
+    }
+}
