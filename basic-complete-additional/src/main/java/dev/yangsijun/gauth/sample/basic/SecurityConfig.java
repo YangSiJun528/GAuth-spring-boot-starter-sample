9f2ccc675572105549cb7818825953ac51175352
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-04 12:07:35 +0900

    로직 추가된 basic 예시 완성본 구현

---
+++ basic-complete-additional/src/main/java/dev/yangsijun/gauth/sample/basic/SecurityConfig.java
@@ -0,0 +1,47 @@
+package dev.yangsijun.gauth.sample.basic;
+
+import dev.yangsijun.gauth.configurer.GAuthLoginConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+    private final GAuthLoginConfigurer<HttpSecurity> gauth;
+
+    public SecurityConfig(GAuthLoginConfigurer<HttpSecurity> gAuthLoginConfigurer) {
+        this.gauth = gAuthLoginConfigurer;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(withDefaults())
+                .cors(withDefaults())
+                .authorizeHttpRequests(request -> request
+                        .antMatchers(
+                                "/move/gauth/authorization",
+                                "/move/login/code/gauth"
+                        ).permitAll()
+                        .antMatchers("/auth/me").authenticated()
+                        .antMatchers("/role/student").hasAuthority("GAUTH_ROLE_STUDENT")
+                        .antMatchers("/role/teacher").hasAuthority("GAUTH_ROLE_TEACHER")
+                        .anyRequest().denyAll()
+                )
+                .logout(withDefaults())
+                .apply(gauth
+                        .loginPageUrl("/move/gauth/authorization")
+                        .loginProcessingUrl("/move/login/code/gauth")
+                        .successHandler(new SimpleUrlAuthenticationSuccessHandler("/gauth/login/success"))
+                        .failureHandler(new SimpleUrlAuthenticationFailureHandler("/gauth/login/failure"))
+                );
+        return http.build();
+    }
+}
