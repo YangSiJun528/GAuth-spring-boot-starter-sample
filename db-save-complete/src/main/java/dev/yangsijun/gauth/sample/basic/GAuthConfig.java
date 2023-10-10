4aa07eb93f0aff27b0819d97bfa1b5764bac1b3a
YangSiJun528 <yangsijun5528@gmail.com>
2023-10-10 09:58:29 +0900

    lombok 사용

--- db-save-complete/src/main/java/dev/yangsijun/gauth/sample/basic/GAuthConfig.java
+++ db-save-complete/src/main/java/dev/yangsijun/gauth/sample/basic/GAuthConfig.java
@@ -1,17 +1,15 @@
 package dev.yangsijun.gauth.sample.basic;
 
 import dev.yangsijun.gauth.userinfo.GAuthUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class GAuthConfig {
 
     private final GAuthUserRepository gauthUserRepository;
-
-    public GAuthConfig(GAuthUserRepository gauthUserRepository) {
-        this.gauthUserRepository = gauthUserRepository;
-    }
 
     @Bean
     public GAuthUserService customGAuthUserService() {
